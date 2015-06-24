7d2d8c68d993b268bd79964f80de92d257f736fa
Malwani <bbardroff@googlemail.com>
2015-06-24 14:34:12 +0200

    Some Changes

---
+++ src/testDragon/TestPackage3.java
@@ -0,0 +1,34 @@
+package testDragon;
+
+import net.gc.blazedragon.BlazeDragon;
+import net.gc.blazedragon.BlazePackage;
+
+/**
+ * Created by Benny on 07.03.2015.
+ */
+public class TestPackage3 extends BlazePackage
+{
+    TestPackage3(short ID)                                                                                               // Für BlazeDragon Init
+    {
+        super(ID);
+    }
+
+    TestPackage3()                                                                                                       // Normale Init (BD Init vorrausgesetzt))
+    {
+        super();
+    }
+
+    @Override
+    public void defineBlazeData()
+    {
+        addBlazeDataType(BlazeDragon.BOOLEAN_DATA);
+        addBlazeDataType(BlazeDragon.STRING_DATA);
+        addBlazeDataType(BlazeDragon.STRING_DATA);
+        addBlazeDataType(BlazeDragon.STRING_DATA);
+        addBlazeDataType(BlazeDragon.STRING_DATA);
+        addBlazeDataType(BlazeDragon.STRING_DATA);
+        addBlazeDataType(BlazeDragon.STRING_DATA);
+    }
+
+}
+
